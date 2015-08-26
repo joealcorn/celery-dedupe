3a372030aacbf7a36d34724494f0301597e37db1
Joe Alcorn <joealcorn123@gmail.com>
2015-08-26 09:36:41 +0100

    Improve tests

--- tests/test_task.py
+++ tests/test_task.py
@@ -15,14 +15,15 @@ def noop_task(*a, **kw):
     return None
 
 
-@task(base=DedupeTask, storage=storage)
-def raise_exception(*a, **kw):
+@task(base=DedupeTask, storage=storage, bind=True)
+def raise_exception(self, *a, **kw):
     raise AttributeError()
 
 
 class TestDedupeTask(object):
 
     def setup_class(self):
+        redis.flushdb()
         current_app.config_from_object(settings)
 
     def test_create_key(self):
@@ -41,20 +42,25 @@ class TestDedupeTask(object):
         assert key == 'cd:celery_dedupe.tasks.DedupeTask:271a7d4a14d7892b87b8fc96d50507a9'
 
     def test_apply_task_twice(self):
-        result1 = noop_task.apply_async(countdown=10)
-        result2 = noop_task.apply_async()
+        result1 = noop_task.apply_async(countdown=0.5)
+        result2 = noop_task.delay()
         assert result1.task_id == result2.task_id
+        assert result1.get() == result2.get()
+        assert not redis.exists(noop_task._create_key(None, None))
 
     def test_apply_task_once(self):
         result = noop_task.delay(1)
         assert result.get() == None
+        assert not redis.exists(noop_task._create_key((1,), {}))
 
     def test_key_cleared_on_exception(self):
-        key = raise_exception._create_key(tuple(), dict())
+        key = raise_exception._create_key(None, None)
         assert not redis.exists(key)
         try:
             raise_exception.delay().get()
         except AttributeError:
-            pass
+            assert True
+        else:
+            assert False
 
         assert not redis.exists(key)
