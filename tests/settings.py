95173ce47ffd329ae0aab7df517dba164929c024
Joe Alcorn <joealcorn123@gmail.com>
2015-09-10 23:37:50 +0100

    u

--- tests/settings.py
+++ tests/settings.py
@@ -2,3 +2,4 @@ BROKER_URL = 'redis://localhost:6379/9'
 CELERY_ALWAYS_EAGER = False
 CELERY_IMPORTS = ('tests.test_task',)
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/9'
+CELERY_BACKEND = CELERY_RESULT_BACKEND  # alias for pre celery 3.1.9
